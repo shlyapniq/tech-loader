af058d319b61560f63f2693390fa806e321c456e
dogi12 <88685766+dogi12@users.noreply.github.com>
2023-08-07 23:10:01 +0300

    Update main.lua

--- launcher/main.lua
+++ launcher/main.lua
@@ -1,1 +1,18 @@
+local ui = require("ui")
+local sys = require("sys")
 
+local win = ui.Window("TEST","fixed",250,100)
+
+function wait(time)
+    sys.sleep(time*1000)
+end
+
+
+win:center()
+win:show()
+
+
+
+repeat ui.update()
+    
+until not win.visible
